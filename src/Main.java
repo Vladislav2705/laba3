8672e08c103f2fd3e2371f5c950f35b98b31c6ef
gradbit1 <gradbit1@gmail.com>
2019-01-12 01:09:49 +0600

    lab 3

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+  Array abc = new Array();
+  abc.createArray();
+  System.out.println(abc.strList.get(5));
+  Name abcd = new Name();
+  abcd.mapping();
+  abcd.out(10);
+  abcd.outall();
+
+}
+}
+
